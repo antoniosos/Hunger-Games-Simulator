580b130f914c599bd787973fa130df5670d5a5e1
antoniosos <67897276+antoniosos@users.noreply.github.com>
2025-06-05 15:59:00 +0200

    adding and deleting characters in character creation

--- character-creation/characterCreation.js
+++ character-creation/characterCreation.js
@@ -1,12 +1,13 @@
 $(document).on('click', '.add_district', function() {
+    console.log("new button pressed");
   $(`<div class="district">
                 <p>District</p>
                 <div class="character">
                     <input type="text" placeholder="name">
                     <select name="gender" id="gender">
+                        <option value="Mystery">Mystery</option>
                         <option value="M">M</option>
                         <option value="F">F</option>
-                        <option value="Mystery">Mystery</option>
                     </select>
                         
                 </div>
@@ -14,10 +15,27 @@ $(document).on('click', '.add_district', function() {
                 <div class="character">
                     <input type="text" placeholder="name">
                     <select name="gender" id="gender">
+                        <option value="Mystery">Mystery</option>
                         <option value="M">M</option>
                         <option value="F">F</option>
-                        <option value="Mystery">Mystery</option>
                     </select>
                 </div>
+                <input type="button" value="Delete district" id="delete_district_button">
             </div>`).insertAfter('.district:last');
 })
+
+$(document).on('click', '#delete_district_button', function() {
+    console.log("delete button pressed");
+    $(this).parent().remove();
+})
+
+$(document).on('click', '#delete_district_first_button', function() {
+    
+    $(this).parent().children('.character').children('input').val('');
+    $(this).parent().children('.character').children('#gender').val('Mystery').change();
+})
+
+$(document).on('click', '#next_button', function() {
+    $()
+})
+
